816593a96280086bcc43db742e71b985a2d364f7
rajBopche <rajbopche@neosofttech.com>
2019-10-18 20:55:23 +0530

    added private member

--- app/src/main/java/com/example/firebaseplayground/ui/ui/MainActivity.kt
+++ app/src/main/java/com/example/firebaseplayground/ui/ui/MainActivity.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val fbAuth = FirebaseAuth.getInstance()
-    val dbRef = FirebaseDatabase.getInstance().reference
+    private val dbRef = FirebaseDatabase.getInstance().reference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
